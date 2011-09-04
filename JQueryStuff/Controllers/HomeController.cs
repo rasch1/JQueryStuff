1d55082cd4708cf692b35ba099900fd35f5aff41
Frank Rasch <rasch101@gmail.com>
2011-09-04 11:38:37 +0200

    jquery first commit

---
+++ JQueryStuff/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JQueryStuff.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hej";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            // Verify that the user selected a file
+            if (file != null && file.ContentLength > 0)
+            {
+                // extract only the fielname
+                var fileName = Path.GetFileName(file.FileName);
+                // store the file inside uploads folder
+                var path = Path.Combine(Server.MapPath("~/Uploads"), fileName);
+                file.SaveAs(path);
+            }
+            // redirect back to the index action to show the form once again
+            return RedirectToAction("Index");
+        }
+
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Fading()
+        {
+            return View();
+        }
+        public ActionResult SlidingPage()
+        {
+            return View();
+        }
+        public ActionResult Left()
+        {
+            return View("~/Views/Pages/Left.cshtml");
+        }
+        public ActionResult Right()
+        {
+            return View("~/Views/Pages/Left.cshtml");
+        } 
+        public ActionResult Modal()
+        {
+            return View("~/Views/Pages/Modal.cshtml");
+        } 
+        public ActionResult Font()
+        {
+            return View();
+        }
+        public ActionResult Editor()
+        {
+            return View();
+        }
+    }
+}
---
+++ JQueryStuff/Global.asax.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace JQueryStuff
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+               "fadingroute", // Route name
+               "fading", // URL with parameters
+               new { controller = "Home", action = "Fading", id = UrlParameter.Optional } // Parameter defaults
+           );
+            routes.MapRoute("slidingroute", "sliding",new { controller = "Home", action = "SlidingPage", id = UrlParameter.Optional });
+            routes.MapRoute("leftslideroute", "left",new { controller = "Home", action = "Left", id = UrlParameter.Optional });
+            routes.MapRoute("rightslideroute", "right",new { controller = "Home", action = "Right", id = UrlParameter.Optional });
+            routes.MapRoute("modalslideroute", "modal",new { controller = "Home", action = "Modal", id = UrlParameter.Optional });
+            routes.MapRoute("textroute", "text",new { controller = "Home", action = "Font", id = UrlParameter.Optional });
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
